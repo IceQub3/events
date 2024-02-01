9b97b66240289178f85549171d8508d27c7b8380
Yuval Sivan <ice_qub3@protonmail.com>
2024-02-01 18:39:47 +0200

    breaking change: removed context support

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,61 +1,32 @@
 import type { Subscriber, Topic } from "./topic";
 
-
 /**
  * @public
  */
-export class Publisher<Arguments extends any[], ThisConstraint = {} | undefined> implements Topic<Arguments, ThisConstraint> {
-    private freeSubscribers = new Set<Subscriber<Arguments, undefined>>();
-    private boundSubscribers = new Map<unknown, Set<Subscriber<Arguments, ThisConstraint | undefined>>>()
+export class Publisher<Arguments extends any[]> implements Topic<Arguments> {
+  private subscribers = new Set<Subscriber<Arguments>>();
 
-    constructor(
-        private readonly options: {
-            handleError?(error: unknown, context?: unknown): void
-        }
-    ) {}
-    
-    subscribe<ThisArgument extends ThisConstraint = undefined extends ThisConstraint ? undefined : never>(
-        subscriber: Subscriber<Arguments, ThisArgument>,
-        context?: ThisArgument
-    ): void {
-        (context ? this.getOrMakeBoundSet(context) : this.freeSubscribers).add((subscriber as Subscriber<Arguments, ThisConstraint | undefined>));
+  constructor(
+    private readonly options: {
+      handleError?(error: unknown, context?: unknown): void;
+    },
+  ) {}
+
+  subscribe(subscriber: Subscriber<Arguments>): void {
+    this.subscribers.add(subscriber);
+  }
+
+  unsubscribe(subscriber: Subscriber<Arguments>): void {
+    this.subscribers.delete(subscriber);
+  }
+
+  emit(...args: Arguments): void {
+    for (const subscriber of this.subscribers) {
+      try {
+        subscriber.apply(undefined, args);
+      } catch (error: unknown) {
+        this.options.handleError?.call(undefined, error);
+      }
     }
-
-    unsubscribe<Context>(
-        subscriber: Subscriber<Arguments, Context>,
-        context?: Context
-    ): void {
-        (context ? this.boundSubscribers.get(context) : this.freeSubscribers)?.delete((subscriber as Subscriber<Arguments, unknown>));
-    }
-    
-    emit(...args: Arguments): void {
-        for (const subscriber of this.freeSubscribers) {
-            try {
-                subscriber.apply(undefined, args);
-            } catch(error: unknown) {
-                this.options.handleError?.call(undefined, error);
-            }
-        }
-
-        for (const [context, subscribers] of this.boundSubscribers) {
-            for (const subscriber of subscribers) {
-                try {
-                    subscriber.apply(context as ThisConstraint, args);
-                } catch (error: unknown) {
-                    this.options.handleError?.call(undefined, error, context);
-                }
-            }
-        }
-    }
-
-    private getOrMakeBoundSet(context: unknown) {
-        let set = this.boundSubscribers.get(context);
-
-        if (!set) {
-            set = new Set();
-            this.boundSubscribers.set(context, set);
-        }
-
-        return set;
-    }
+  }
 }
--- src/topic.ts
+++ src/topic.ts
@@ -1,24 +1,15 @@
-
 /**
  * @public
  */
-export type Subscriber<
-    Arguments extends any[],
-    ThisArgument = undefined
-> = (this: ThisArgument, ...args: Arguments) => void | PromiseLike<void>;
+export type Subscriber<Arguments extends any[]> = (
+  ...args: Arguments
+) => void | PromiseLike<void>;
 
 /**
  * @public
  * A topic of events
  */
-export interface Topic<Arguments extends any[], ThisConstraint = {} | undefined> {
-    subscribe<ThisArgument extends ThisConstraint = undefined extends ThisConstraint ? undefined : never>(
-        subscriber: Subscriber<Arguments, ThisArgument>,
-        context?: ThisArgument
-    ): void;
-
-    unsubscribe<Context extends ThisConstraint = undefined extends ThisConstraint ? undefined : never>(
-        listener: Subscriber<Arguments, Context>,
-        context?: Context
-    ): void
+export interface Topic<Arguments extends any[]> {
+  subscribe(subscriber: Subscriber<Arguments>): void;
+  unsubscribe(listener: Subscriber<Arguments>): void;
 }
